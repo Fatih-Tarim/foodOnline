101f6d71b0c55b77754d831ac8b5b48c6bf94ebf
Fatih-Tarim <fatihtrm99@gmail.com>
2023-11-20 12:09:58 +0300

    UserProfile Model & CustomUserAdmin registration from admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
-from accounts.models import User
-from django.contrib.auth.admin import UserAdmin
+from accounts.models import User, UserProfile
+from django.contrib.auth.admin import UserAdmin 
+from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserAdmin(UserAdmin):
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
-    list_display = ('email','username','first_name','last_name','role','is_admin','is_active')
+    list_display = ('email','username','first_name','last_name','role','is_active','is_admin')
     ordering = ('-date_joined',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'first_name', 'last_name','phone_number'),
+        }),
+    )
+
+
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)

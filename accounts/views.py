75f3d22cc0eaa2f266b1d0b53583e96a3a2b62e0
Fatih-Tarim <fatihtrm99@gmail.com>
2023-11-20 20:16:13 +0300

    Template Inheritance | Seperate html files

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def registerUser(request):
+    return HttpResponse("registerUser")
+
+def registerRestaurant(request):
+    return HttpResponse("registerRestaurant")

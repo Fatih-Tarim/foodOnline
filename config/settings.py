5c7ef012488f7d23ebfb1b2a4f416dfd7c357ba9
Fatih-Tarim <fatihtrm99@gmail.com>
2023-11-20 12:19:45 +0300

    Media Files Configuration

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,11 @@ STATICFILES_DIRS= [
     'config/static'
 ]
 
+#Media Files Conf
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

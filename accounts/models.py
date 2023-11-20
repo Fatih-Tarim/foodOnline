be50b404087faebeb38b1a40c3bfdafa6ae7666c
Fatih-Tarim <fatihtrm99@gmail.com>
2023-11-20 18:34:33 +0300

    Signals | Create UserProfile when user register

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class UserManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
---
+++ accounts/signals.py
@@ -0,0 +1,28 @@
+from accounts.models import User, UserProfile
+
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+
+
+
+@receiver(post_save, sender=User)
+def post_save_create_profile_reciever(sender, instance, created,**kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+        print('User profile is created')
+    
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
+            #Create the profile if not exist
+            UserProfile.objects.create(user=instance)
+            print('Profile was not exist, but i created one :)')
+        print('User Profile Updated')
+
+@receiver(pre_save, sender=User)
+def pre_save_profile_reciever(sender,instance, **kwargs):
+    print(instance.username, 'this user being saved')
+#post_save.connect(post_save_create_profile_reciever, User)
+

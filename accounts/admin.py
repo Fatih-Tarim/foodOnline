3528d2e69c8413068a6c2715a381653ea1a8e651
Fatih-Tarim <fatihtrm99@gmail.com>
2023-11-20 11:11:23 +0300

    CustomUserAdmin | For admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from accounts.models import User
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+    list_display = ('email','username','first_name','last_name','role','is_admin','is_active')
+    ordering = ('-date_joined',)
+
+admin.site.register(User, CustomUserAdmin)

dfe51e42a7aa0ed800609786f079a46c29548db0
Fatih-Tarim <fatihtrm99@gmail.com>
2023-11-23 14:59:38 +0300

    Password hash with create_user() method

--- accounts/views.py
+++ accounts/views.py
@@ -5,15 +5,25 @@ from .models import User
 
 def registerUser(request):
     if request.method == 'POST':
-        print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
+            #CREATE USER using  form
+            # password = form.cleaned_data['password'] 
+            # user = form.save(commit=False)
+            # user.role = User.CUSTOMER
+            # user.set_password(password)
+            # form.save()
+
+            #Create the user using create_user() method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            
-            user = form.save(commit=False)
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
-            user.set_password(password)
-            form.save()
+            user.save()
+            print('User is created')
             return redirect('registerUser')
         
     else:

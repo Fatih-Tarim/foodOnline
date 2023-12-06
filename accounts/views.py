1f0ee5c19afbf87aaa2d3e6df5098611e0a2c3eb
Fatih-Tarim <fatihtrm99@gmail.com>
2023-12-06 15:08:59 +0300

    Reset Password Feature Done

--- accounts/views.py
+++ accounts/views.py
@@ -190,7 +190,7 @@ def forgot_password(request):
 
             #Send reset password email.
             mail_subject = 'FoodOnline - Reset password'
-            email_template = 'accounts/emails/account_verification_email.html'
+            email_template = 'accounts/emails/reset_password_email.html'
             send_verification_email(request, user, mail_subject, email_template)
 
             messages.success(request, 'Password reset link has been send your email address.')
@@ -203,10 +203,39 @@ def forgot_password(request):
 
 def reset_password_validate(request, uidb64, token):
     #validate the user by decoding the token and user pk
-    pass
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.info(request, "Please reset your password")
+        return redirect('reset_password')
+    else:
+        messages.error(request, "This link has been expired.")
+        return redirect('myAccount')
      
 
 def reset_password(request):
+    if request.method == 'POST':
+        #field name -> password
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            #This give me primary key of the user
+            pk = request.session.get('uid')
+            user = User.objects.get(pk=pk)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            messages.success(request, "Password reset successful")
+            return redirect("login")
+        else:
+            messages.error(request, "Password do not match!")
+            return redirect('reset_password')
+
     return render(request, 'accounts/reset_password.html')
 
 

dcad1e131f94f38f3ae6d846fadf7a9cfe8206a7
Fatih-Tarim <fatihtrm99@gmail.com>
2023-12-08 17:42:09 +0300

    VendorDashboard dynamic fields

--- accounts/views.py
+++ accounts/views.py
@@ -180,7 +180,11 @@ def customerDashboard(request):
 @login_required(login_url="login")
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-    return render(request, "accounts/vendorDashboard.html")
+    vendor = Vendor.objects.get(user=request.user)
+    context = {
+        'vendor': vendor,
+    }
+    return render(request, "accounts/vendorDashboard.html", context=context)
 
 def forgot_password(request):
     if request.method == "POST":

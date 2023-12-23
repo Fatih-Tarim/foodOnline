6cc205ed07213cbc7e20757ca528d44a8d676981
Fatih-Tarim <fatihtrm99@gmail.com>
2023-12-23 17:21:51 +0300

    Homepage edits

--- config/views.py
+++ config/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#Vendor App
+from vendor.models import Vendor
+
 def home(request):
-    return render(request, template_name='home.html')
+    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+    context={
+        'vendors': vendors,
+    }
+    return render(request, template_name='home.html', context=context)

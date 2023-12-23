6744193aa31e2884bdc8ab25a3ff0377a73c225a
Fatih-Tarim <fatihtrm99@gmail.com>
2023-12-23 19:49:00 +0300

    Markeplace | Vendor detail page setup | Part III

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 
 #Vendor App
 from vendor.models import Vendor
+
+#Menu App
 
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -11,3 +14,11 @@ def marketplace(request):
         'vendor_count': vendor_count,
     }
     return render(request, "marketplace/listings.html", context=context)
+
+def vendor_detail(request, vendor_slug=None):
+    # vendor = Vendor.objects.filter(vendor_slug=vendor_slug)
+    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    context= {
+        'vendor': vendor,
+    }
+    return render(request, 'marketplace/vendor_detail.html', context)

73677328e3b90c579d731eb44875d2026c19f419
Fatih-Tarim <fatihtrm99@gmail.com>
2023-12-23 21:05:14 +0300

    fooditems vendor-detail | Markeplace End

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
+from django.db.models import Prefetch
 
 #Vendor App
 from vendor.models import Vendor
+from vendor.utils import get_vendor
 
 #Menu App
+from menu.models import Category, FoodItem
 
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -16,9 +19,16 @@ def marketplace(request):
     return render(request, "marketplace/listings.html", context=context)
 
 def vendor_detail(request, vendor_slug=None):
-    # vendor = Vendor.objects.filter(vendor_slug=vendor_slug)
     vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset=FoodItem.objects.filter(is_available=True)
+        )
+    )
     context= {
         'vendor': vendor,
+        'categories': categories,
+        # 'foods': foods,
     }
     return render(request, 'marketplace/vendor_detail.html', context)

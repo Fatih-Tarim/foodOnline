4f36a8e8f2b044f53cbeec758a4330c0b169653d
Fatih-Tarim <fatihtrm99@gmail.com>
2024-01-08 09:47:12 +0300

    Opening hours list frontend | Openin Hour Feature

--- marketplace/views.py
+++ marketplace/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.db.models import Prefetch
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from datetime import date
 
 #Gis
 from django.contrib.gis.geos import GEOSGeometry
@@ -11,7 +12,7 @@ from django.contrib.gis.measure import D
 from django.contrib.gis.db.models.functions import Distance
 
 #Vendor App
-from vendor.models import Vendor
+from vendor.models import Vendor, OpeningHour
 from vendor.utils import get_vendor
 
 #Menu App
@@ -41,6 +42,11 @@ def vendor_detail(request, vendor_slug=None):
             queryset=FoodItem.objects.filter(is_available=True)
         )
     )
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', '-from_hour')
+    #Check current day's opening hours
+    today_date = date.today()
+    today = today_date.isoweekday()
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
     else:
@@ -49,6 +55,8 @@ def vendor_detail(request, vendor_slug=None):
         'vendor': vendor,
         'categories': categories,
         'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hours': current_opening_hours
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 

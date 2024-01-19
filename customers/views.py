cca2b69d285f82541144fdaf0073d1ae7305a7e7
Fatih-Tarim <fatihtrm99@gmail.com>
2024-01-19 13:45:03 +0300

    account app forms

--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
+#Accounts App
+from accounts.forms import UserProfileForm, UserInfoForm
+from accounts.models import UserProfile
+
+@login_required(login_url='login')
 def cprofile(request):
-    return render(request, "customers/cprofile.html")
+    profile = get_object_or_404(UserProfile, user=request.user)
+    profile_form = UserProfileForm(instance=profile)
+    user_form = UserInfoForm(instance=request.user)
+    context = {
+        'profile_form': profile_form,
+        'user_form': user_form,
+    }
+    return render(request, "customers/cprofile.html", context)
